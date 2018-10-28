412e986c72179f12800ceeb86482f58b729aa9eb
sa1987 <aswathi.tcs@gmail.com>
2018-10-28 23:44:44 +0530

    Create job-perl.sh

---
+++ job-perl.sh
@@ -0,0 +1,18 @@
+cat << EOF > job-pi.yaml
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: pi
+spec:
+  template:
+    spec:
+      containers:
+      - name: pi
+        image: perl
+        command: ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+      restartPolicy: Never
+  backoffLimit: 4
+  EOF
+  
+  
+  kubectl create -f pi-job.yaml
